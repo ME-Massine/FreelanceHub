0913e59e2f8122e1d280ac0e298a7fa5fef12683
MsdAmine <moussaid.amine19@gmail.com>
2025-05-05 22:42:28 +0100

    Fetching and displaying freelancers data from db

--- freelancer/views.py
+++ freelancer/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Freelancer, Service
 
 
 # Create your views here.
@@ -12,10 +13,13 @@ def logout_view(request):
     return redirect('clientPage')
 
 def settings(request):
-    return render(request, 'client/settings.html')
+    return render(request, 'freelancer/settings.html')
 
 @login_required
 def dashboard(request):
     name = request.user.username
     return render(request, 'freelancer/dashboard.html', {'name': name})
 
+def profile(request):
+    freelancerinfo= get_object_or_404(Freelancer, user=request.user)
+    return render(request, 'freelancer/profile.html', {'freelancerinfo': freelancerinfo})
