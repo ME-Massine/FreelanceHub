4d96c333efe40e9b33022d72da688a678ab540b0
ME-Massine <massine000@gmail.com>
2025-05-17 01:50:28 +0100

    fixed the styling for the mission cards

--- client/models.py
+++ client/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=255, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
+    company_name = models.CharField(max_length=255, default='not provided')
+    phone_number = models.CharField(max_length=20, default='not provided')
     address = models.TextField(blank=True)
 
 
